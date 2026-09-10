dcc12ca94c814ec8fc64acd3491918fa8934d470
gakkinkoo <jakub.lucivjansky9@gmail.com>
2026-09-10 11:24:58 +0200

    start

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "haha";?>
+
+    <?php
+    echo "hihi";?>
+
+    <?php echo "<p>vnutro php</p>";?>
+   
+   <?php print("printed");
+    ?>
+
+</body>
+</html>
